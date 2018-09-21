6081ba25e9a7206b7328a0deaad4cec8a66137ef
Ajay Nikumbh <37560890+ajay-nikumbh@users.noreply.github.com>
2018-09-21 08:38:09 +0530

    java sorting codes

--- BubbleSort1.java
+++ BubbleSort1.java
@@ -1,4 +1,4 @@
-		//Program for Bubble sort in ascending order
+		//Program for Bubble sort in descending order
 import java.util.*;
 class BubbleSort1
 {
@@ -7,7 +7,7 @@ class BubbleSort1
         int n = arr.length;
         for (int i = 0; i < n-1; i++)
             for (int j = 0; j < n-i-1; j++)
-                if (arr[j] > arr[j+1])
+                if (arr[j] < arr[j+1])
                 {
                     // swap temp and arr[i]
                     int temp = arr[j];
